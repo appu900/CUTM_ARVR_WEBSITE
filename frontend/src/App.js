613e071c21d0a94b04d9a760d14d6d47f99e8938
pabitra <89703515+appu900@users.noreply.github.com>
2023-08-06 02:17:23 +0530

    react-app-created and tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-800">
+     <p className='text-white sm:text-green-600 md:text-red-600'>hello centurion</p>
+    </div>
+  );
+}
+
+export default App;
